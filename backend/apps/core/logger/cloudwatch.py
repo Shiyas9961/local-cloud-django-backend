e38d6ea58abbba6147a585ea7e4f37ee4272ce67
Shiyas9961 <shiyasshiyyu9961@gmail.com>
2025-12-13 14:15:18 +0530

    feat: docker-compose.yml file changes and make cloudwatch handler safe mode

--- backend/apps/core/logger/cloudwatch.py
+++ backend/apps/core/logger/cloudwatch.py
@@ -1,5 +1,7 @@
 """Cloudwatch logger."""
 
+import logging
+
 import boto3
 import watchtower
 from django.conf import settings
@@ -10,20 +12,26 @@ class LocalStackHandler(watchtower.CloudWatchLogHandler):
 
     def __init__(self, *args, **kwargs):
         """Initialize the handler."""
-        session = boto3.session.Session(
-            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            region_name=settings.AWS_REGION_NAME,
-        )
+        try:
 
-        super().__init__(
-            log_group_name=settings.AWS_CLOUDWATCH_LOG_GROUP,
-            log_stream_name=settings.AWS_CLOUDWATCH_STREAM_NAME,
-            use_queues=False,
-            create_log_group=True,
-            # no boto3_session here!!
-            boto3_client=session.client(
-                "logs",
-                endpoint_url=settings.AWS_ENDPOINT_URL,  # LocalStack
-            ),
-        )
+            session = boto3.session.Session(
+                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                region_name=settings.AWS_REGION_NAME,
+            )
+
+            super().__init__(
+                log_group_name=settings.AWS_CLOUDWATCH_LOG_GROUP,
+                log_stream_name=settings.AWS_CLOUDWATCH_STREAM_NAME,
+                use_queues=False,
+                create_log_group=True,
+                # no boto3_session here!!
+                boto3_client=session.client(
+                    "logs",
+                    endpoint_url=settings.AWS_ENDPOINT_URL,  # LocalStack
+                ),
+            )
+        except Exception as e:
+            logging.getLogger(__name__).error(
+                f"Failed to initialize CloudWatch handler: {e}"
+            )
